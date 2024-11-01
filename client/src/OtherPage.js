c171b07625a6f5554934c5c57a13337b3acb98bd
slince-zero <1711257704@qq.com>
2024-11-01 17:48:19 +0800

    init

---
+++ client/src/OtherPage.js
@@ -0,0 +1,11 @@
+/* eslint-disable import/no-anonymous-default-export */
+import { Link } from 'react-router-dom'
+
+export default () => {
+  return (
+    <>
+      <p>Other Page</p>
+      <Link to='/'>Home</Link>
+    </>
+  )
+}
