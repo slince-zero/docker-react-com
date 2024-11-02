6dd042766c4d280efffe4847ffae8c13bded1397
slince-zero <1711257704@qq.com>
2024-11-02 16:31:42 +0800

    chore

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import OtherPage from './OtherPage';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Fib from './Fib';
+import OtherPage from './OtherPage';
+import logo from './logo.svg';
 
 function App() {
   return (
-      <Router>
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to React</h1>
-            <Link to="/">Home</Link>
-            <Link to="/otherpage">Other Page</Link>
-          </header>
-          <div>
-            <Route exact path="/" component={Fib} />
-            <Route path="/otherpage" component={OtherPage} />
-          </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+          <Link to="/">Home</Link>
+          <Link to="/otherpage">Other Page</Link>
+        </header>
+        <div>
+          <Routes>
+            <Route path="/" element={<Fib />} />
+            <Route path="/otherpage" element={<OtherPage />} />
+          </Routes>
         </div>
-      </Router>
+      </div>
+    </Router>
   );
 }
 
